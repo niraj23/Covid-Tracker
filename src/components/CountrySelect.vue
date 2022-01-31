9751155a88c057eaa29e0669e73605aa389e256f
Niraj <niraj2323@gmail.com>
2022-01-31 11:02:17 -0500

    fixed select country issue

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -4,7 +4,7 @@
     class="form-select block w-full border p-3 rounded flex-1"
     @change="onChange()"
   >
-    <option value="0">Select Country</option>
+    <option value="0" selected="true" disabled="disabled">Select Country</option>
     <option v-for="country in countries" :value="country.ID" v-bind:key="country.ID"
     >
       {{ country.Country }}
